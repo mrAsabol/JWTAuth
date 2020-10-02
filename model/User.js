8b8bfd7536f590b5cd5cd344e71e32402a9579c3
PhysoTronic <26413093+PhysoTronic@users.noreply.github.com>
2020-10-02 07:48:28 +0200

    Update User.js
    Comments

--- model/User.js
+++ model/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema ({
     name: {
         type: String,
-        required: true,
+        required: true, //setting username as required
         min: 6,
         max: 255
     },
