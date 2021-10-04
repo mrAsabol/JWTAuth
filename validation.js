0d1113df359daa571be9dbb835a9497957bf632a
PhysoTronic <26413093+PhysoTronic@users.noreply.github.com>
2021-10-04 18:45:41 +0200

    Added backup email

--- validation.js
+++ validation.js
@@ -6,7 +6,8 @@ const registerValidation = (data) => {
     const schema = {
         name: Joi.string().min(6).required(),
         email: Joi.string().min(6).required().email(),
-        password: Joi.string().min(6).required()
+        password: Joi.string().min(6).required(),
+        backupemail: Joi.string().min(6).required().email()
     };
     return Joi.validate(data, schema);
 }
